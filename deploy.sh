960a42d643c2133ca08be1777dad471f27ecb724
lepe <dev@alepe.com>
2014-01-28 13:15:14 +0900

    added DEPLOY_DIR for easy customization

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 make
-#VERSION=$(cat /var/games/trem_src/trem-pbot/version);
-#VERSION=$[$VERSION+1];
-#echo $VERSION > /var/games/trem_src/trem-pbot/version
-#VERSION=$(printf "%03d" $VERSION);
+DEPLOY_DIR=/var/games/tremulous/pbot-gpp
 VERSION=0;
-PK3VM=pbot-vms-0.$VERSION.pk3
-cp build/release-linux-x86_64/base/vm/*.qvm /var/games/tremulous/pbot/vm/
+PK3VM=pbot-gpp-vms-0.$VERSION.pk3
+cp build/release-linux-x86_64/base/vm/*.qvm $DEPLOY_DIR/vm/
 cp build/release-linux-x86_64/base/vm/cgame.qvm deploy/pbot-vm/vm/
 cp build/release-linux-x86_64/base/vm/ui.qvm deploy/pbot-vm/vm/
 cd deploy/pbot-vm/
 zip -r $PK3VM .
-mv $PK3VM /var/games/tremulous/pbot/
+mv $PK3VM $DEPLOY_DIR
